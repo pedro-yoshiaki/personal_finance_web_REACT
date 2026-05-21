3cc060ed04cfd2e91845d6bfcb252caf06ea8df2
Pedro Yoshiaki <pedro.yoshiaki@gmail.com>
2026-05-21 17:48:10 -0300

    Design Footer

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -4,7 +4,8 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="app-footer">
-      <p>Projeto Web II - Desenvolvido em dupla</p>
+      <p className="footer-project">Projeto Dinâmica para Web &mdash; Feito em Dupla</p>
+      <p className="footer-copy">&copy; {new Date().getFullYear()} Matheus Nascimento &amp; Pedro Nohara</p>
     </footer>
   );
 }
